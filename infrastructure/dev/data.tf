49d984eb1a721b52474b31e7627d7b0a235e48b3
bwieckow <bartlomiej.wieckowski@circlekeurope.com>
2025-06-17 14:42:02 +0200

    Resolves #25 - create dev environment, resolves #25

--- infrastructure/dev/data.tf
+++ infrastructure/dev/data.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "../src"
+  source_dir  = "../../src"
   output_path = "lambda_function.zip"
 }
 
@@ -11,6 +11,6 @@ data "archive_file" "lambda_zip" {
 #   name = "/ops-master/cloudfront/distribution_id"
 # }
 
-data "aws_cloudfront_distribution" "opsmaster" {
-  id = data.aws_ssm_parameter.cloudfront_distribution_id.value
-}
+# data "aws_cloudfront_distribution" "opsmaster" {
+#   id = data.aws_ssm_parameter.cloudfront_distribution_id.value
+# }
